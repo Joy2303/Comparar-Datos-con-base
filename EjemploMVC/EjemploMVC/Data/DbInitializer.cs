97abb5428f635b240755f3e2db4e906fcb4319e4
Joy2303 <47402265+Joy2303@users.noreply.github.com>
2019-02-06 22:08:36 -0500

    Initial Commit

---
+++ EjemploMVC/EjemploMVC/Data/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EjemploMVC.Models;
+
+
+namespace sistema.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(EjemploMVCContext context)
+        {
+            context.Database.EnsureCreated();
+
+            //Buscar registros en la tabla
+
+            if (context.Persona.Any())
+            {
+                return;
+            }
+
+            var consultas = new Persona[]
+            {
+                new Persona{ nombre="Carlos Lopez",fecha="23-03-2019",hora="10:15" }
+            };
+
+            foreach (Persona c in consultas)
+            {
+                context.Persona.Add(c);
+            }
+            context.SaveChanges();
+        }
+
+    }
+
+}
---
+++ EjemploMVC/EjemploMVC/Models/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjemploMVC.Models
+{
+    public class Persona
+    {
+
+        [Key]
+        public int idConsulta { get; set; }
+        [Required(ErrorMessage = "Campo Requerido")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre debe tener de 3 a 50 caracteres")]
+        public string nombre { get; set; }
+
+        public string fecha { get; set; }
+        public string hora { get; set; }
+
+    }
+}
